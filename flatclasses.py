04f97537201abad45e707af032b38666f1b5be0b
David-Casas <dafecato4@gmail.com>
2019-08-28 20:57:24 -0500

    Programado misil, naves tipo guardian, ahora con stripes.

--- flatclasses.py
+++ flatclasses.py
@@ -1,6 +1,9 @@
 import pygame
 from pygame.locals import *
 import math
+import random
+import numpy
+import pygame.surfarray
 
 
 SPEED = 1
@@ -8,14 +11,22 @@ TIME = 1
 MARGIN = 3
 WINH, WINW = 700 +2*MARGIN, 1300 + 2*MARGIN
 BTLFLDH, BTLFLDW = 700, 900
+BASEW, BASEH = 50, 200
 SHIRAD = 20
+SHIPW, SHIPH = 2*SHIRAD,2*SHIRAD
+BULLETRAD = 3
+MISILRAD = 10
 CANNW, CANNH = 12,24
+SHIP_SOURCE = 'resources/ship.png'
+MISIL_SOURCE = 'resources/misil.png'
+
+RANGE = 100
 
 BLACK = (0,0,0,0)
 GRAY = (150,150,150,0)
 DEEPBLUE = (0,4,130,0)
-
-objectsToDraw = []
+#Jugada arriesgada y no muy pythonica para establecer ids para cada uno.
+inGame = []
 
 class spaceObject:
     """Clase principal de objeto.
@@ -27,42 +38,90 @@ class spaceObject:
     pos = (0,0)
     v_x = 0
     v_y = 0
-    time_x = 0
-    time_y = 0
     def __init__(self,newPos = None):
         if newPos == None:
             #Siempre iniciará quieto por el momento.
             self.pos = (0,0)
         else:
             self.pos = newPos
-    
-    
+        self.id = (self.pos[0] +random.randint(1, RANGE) ,self.pos[1] + random.randint(1,RANGE))
+        self.rect = ((0,0),(0,0))
+        self.shooted = False
+        self.posX = self.pos[0]
+        self.posY = self.pos[1]
+    #Definir igualdad.
+    def __eq__(self, other):
+        return self.id == other.id
+    def __ne__(self, other):#Recomiendan definir esto también.
+        return self.id != other.id
+    def getX(self):
+        return self.posX
+    def setX(self, newX):
+        self.posX = newX
+    def getY(self):
+        return self.posY
+    def setX(self, newX):
+        self.posY = newY
     def getPos(self):
-        return self.pos
+        return (self.posX, self.posY)
     def setPos(self,newX, newY):
-        self.pos = (newX, newY)
+        self.posX, self.posY = newX, newY
     def getVX(self):
         return self.v_x
     def setVX(self,newVX):
-        self.v_x = newVX
+        if self.shooted:#Si le han disparado no puede cambiar.
+            pass
+        else:
+            self.v_x = newVX
     def getVY(self):
         return self.v_y
     def setVY(self,newAY):
-        self.v_y = newAY
+        if self.shooted:#Si le han disparado no puede cambiar.
+            pass
+        else:
+            self.v_y = newAY
     def getV(self):
         return (self.v_x,self.v_y)
     def setV(self, newAX, newAY):
-        self.v_x,self.v_y = newAX, newAY
+        if self.shooted:
+            pass
+        else:
+            self.v_x,self.v_y = newAX, newAY
+    def getRect(self):
+        return pygame.Rect(self.rect)
+    def setRect(self, newRect):
+        self.rect = pygame.Rect(newRect)    
     def calcPos(self):
-        self.pos = (int(self.pos[0] + (self.v_x)),int(self.pos[1]+(self.v_y)))
-    def velUp(self):
-        self.v_y = self.v_y -SPEED
-    def velDown(self):
-        self.v_y = self.v_y + SPEED
-    def velRight(self):
-        self.v_x = self.v_x +SPEED
-    def velLeft(self):
-        self.v_x = self.v_x -SPEED
+        self.posX, self.posY = int(self.posX + (self.v_x)),int(self.posY+(self.v_y))
+    def velUp(self, speed):
+        if self.shooted:
+            pass
+        else:
+            self.v_y = self.v_y - speed
+    def velDown(self, speed):
+        if self.shooted:
+            pass
+        else:
+            self.v_y = self.v_y + speed
+    def velRight(self, speed):
+        if self.shooted:
+            pass
+        else:
+            self.v_x = self.v_x + speed
+    def velLeft(self, speed):
+        if self.shooted:
+            pass
+        else:
+            self.v_x = self.v_x - speed
+    def switchShooted(self):
+        #print('Impacto!!')
+        self.shooted = not self.shooted
+    def getShooted(self):
+        return self.shooted
+        
+    def isOut(self):
+        if not(0 <= self.posX <= BTLFLDW) or not(0<= self.posY <= BTLFLDH):
+            inGame.remove(self)
     dictAccs = {pygame.K_w: velUp, pygame.K_s: velDown, pygame.K_d: velRight, pygame.K_a: velLeft}
 
 class bullet(spaceObject):
@@ -81,6 +140,8 @@ class bullet(spaceObject):
             
         spaceObject.__init__(self,newPos)
         
+        
+        
         objtvx, objtvy =newObjtv[0], newObjtv[1]
         px, py = self.pos[0], self.pos[1]
         dis = math.sqrt((objtvx - px)**2+(objtvy - py)**2)
@@ -88,41 +149,169 @@ class bullet(spaceObject):
         
         self.setV(self.vel*cosA,self.vel*sinA)
         
-    def draw(self, surf, backSurf):
-        pygame.draw.circle(surf, (255,255,255), self.pos, 3)
-        backSurf.blit(surf, (MARGIN, MARGIN))
-        pygame.display.flip()
+    def draw(self, surf):
+        self.calcPos()
+        self.setRect(pygame.draw.circle(surf, (255,255,255), self.getPos(), BULLETRAD))
+        self.isOut()
+        return self.getRect()
+
+class misil( bullet, pygame.sprite.Sprite):
+    """Clase misil para destruir la nave."""
+    def __init__(self, newPos = None, newVel = None, newObjtv =None):
+        bullet.__init__(self,newPos, newVel, newObjtv)
+        self.image = pygame.transform.scale(pygame.image.load(MISIL_SOURCE).convert_alpha(), (MISILRAD*2, MISILRAD*2))
         
-class ship(spaceObject):
+        
+    def update(self):
+        if self.getShooted():
+            return   
+        for thing in inGame:
+            if self.getRect().colliderect(thing.getRect()) and self != thing:
+                self.switchShooted()
+                inGame.remove(self)
+                if type(thing) is base:
+                    print('Impacto base')
+            
+    def draw(self, surf):
+        self.update()
+        if self.getShooted():
+            surf.blit(self.image, self.getPos())
+            self.setRect((self.getPos(), (MISILRAD*2, MISILRAD*2)))
+            self.isOut()
+            return self.getRect()
+        else:
+            self.calcPos()
+            surf.blit(self.image, self.getPos())
+            self.setRect((self.getPos(), (MISILRAD*2, MISILRAD*2)))
+            self.isOut()
+            return self.getRect()
+        
+class ship(spaceObject, pygame.sprite.Sprite):
     '''Clase nave. Hereda de spaceObject
      '''
-    color = (0,0,0,0)
-    cannRect = ((0,0), (0,0))
     def __init__(self, newPos = None, newColor = None):
-        if newColor == None:
-            pass
-        else:
-            self.color = newColor
-        
+        super().__init__()
+        self.hasMisil = True
+        self.pos = newPos
+        self.color = newColor        
         spaceObject.__init__(self,newPos)
+        self.setRect((self.getX(), self.getY(), SHIPW, SHIPH))
+        self.image = pygame.image.load(SHIP_SOURCE).convert_alpha()
+        self.pixArr =  pygame.surfarray.pixels3d(self.image)
+        #Cambiar para el color
+        for xcor in range(len(self.pixArr[1])):#Recorre las filas.
+            for ycor in range(len(self.pixArr[2])):#Columnas
+                if (self.pixArr[xcor][ycor] == [255,255,255]).all():#Si es blanco le cambia al color.
+                    self.pixArr[xcor][ycor] = self.color
+                elif (self.pixArr[xcor][ycor] == [0,0,0]).all():
+                    self.pixArr[xcor][ycor] = [0,255,0]
+                #print(self.pixArr[xcor][ycor] == [255,255,255])
+        del self.pixArr
+        inGame.append(self)
         
+    def update(self, objective):#Son las coordenadas donde debe apuntar.
         
-
-
-    def update(self):
-        mpx, mpy = pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1]#Not clean
-        posx, posy = self.pos[0], self.pos[1]
+        mpx, mpy = objective[0],objective[1]#Not clean
+        posx, posy = self.getRect().centerx, self.getRect().centery
         dis = math.sqrt((mpx - posx)**2 + (mpy - posy)**2)
         sinA = (mpy -posy)/dis
         cosA = (mpx - posx)/dis
-        return (int(posx +SHIRAD*cosA),int( posy + SHIRAD*sinA))
+        
+        
+        return (int(posx + 2*SHIRAD*cosA),int( posy + 2*SHIRAD*sinA))
     
-    def draw(self, surf, backSurf):
-        pygame.draw.circle(surf, self.color, self.pos, SHIRAD)
-        pygame.draw.circle(surf, (255,255,255,0), self.update(), int(SHIRAD/4))
-        backSurf.blit(surf, (MARGIN, MARGIN))
-        pygame.display.flip()
+    def collided(self):
+        #Va a ver si ha chocado con algo
+        toCheck = [x.getRect() for x in inGame if not(x == self)]
+        colided = self.getRect().collidelist(toCheck)
+        if colided != -1 and not self.getShooted():
+            self.switchShooted()
         
-    def shot(self):
-        objectsToDraw.append(bullet(newPos = self.update(), newVel = 5 , newObjtv = pygame.mouse.get_pos()))
+    
+    def draw(self, backSurf):
+        self.collided()
+        self.calcPos()
+        backSurf.blit(self.image, (self.getPos()))
+        self.setRect((self.getPos(),(SHIPW,SHIPH)))
+        self.isOut()
+        return self.getRect()
         
+    def shot(self, objective):
+        inGame.append(bullet(newPos = self.update(objective), newVel = 10 , newObjtv = objective))
+    def misil(self, objective):
+        if self.hasMisil:
+            inGame.append(misil(newPos = self.update(objective), newVel = 3, newObjtv = objective))
+            pixArr =  pygame.surfarray.pixels3d(self.image)
+            #Cambiar para el color
+            for xcor in range(len(pixArr[1])):#Recorre las filas.
+                for ycor in range(len(pixArr[2])):#Columnas
+                    if (pixArr[xcor][ycor] == [0,255,0]).all():#Si es negro le cambia al color.
+                        pixArr[xcor][ycor] = [255,0,0]
+            del pixArr
+            self.hasMisil = False
+        else:
+            pass
+
+
+
+
+class nship(ship):
+    """Clase para nave enemiga. """
+    
+    def __init__(self, newPos = None, newColor = None, newBehav =1):
+        ship.__init__(self, newPos, newColor)
+        self.origin = newPos #Guardar donde nació.
+        self.Behav = newBehav
+        self.setVY(0)
+        
+    def guardian(self):
+        #Si está quieto lo mueve a algún lado.
+        if self.getVY() == 0:
+            newVel = random.choice([1,-1])
+            self.setVY(newVel)               
+        #Va a ir de arriba a abajo.
+        if(math.fabs(self.getY() - self.origin[1])>= 50):
+            self.setVY(-1*self.getVY())
+        #Revisar todas las cosas que están al rededor unos __ pixeles a la redonda y si son misiles o naves enemigas.
+        for thing in inGame:
+            if (math.sqrt((thing.getX() - self.getX())**2 + (thing.getY() - self.getY())**2 ) <=100 and
+                (type(thing) is misil or 
+                ((type(thing) is ship or type(thing) is nship) and thing.color != self.color)) ):
+                self.shot(thing.getPos())
+    #Este tipo de nave 
+    #def killer(self):
+        
+            
+    dictBehav ={1: guardian }
+    def draw(self, backSurf):
+        self.collided()
+        self.dictBehav[self.Behav](self)
+        self.calcPos()
+        backSurf.blit(self.image, (self.getPos()))
+        self.setRect((self.getPos(),(SHIPW,SHIPH)))
+        self.isOut()
+        return self.getRect()
+        
+
+        
+class base(pygame.sprite.Sprite, spaceObject):
+    """Clase para la base de las naves"""
+    
+    def __init__(self, color, coordenadas, isEnemy = False):
+        super().__init__()
+        spaceObject.__init__(self,coordenadas)
+        self.image = pygame.Surface((BASEW, BASEH)).convert()
+        self.color = color
+        self.image.fill(self.color)
+        inGame.append(self)
+        self.isEnemy = isEnemy
+        
+
+    def getRect(self):
+        return pygame.Rect(self.rect)
+        
+    def draw(self, backSurf):
+        self.rect = backSurf.blit(self.image, (self.getX(),self.getY()))
+        return self.rect
+        
+         
--- flatwarsgui.py
+++ flatwarsgui.py
@@ -2,26 +2,36 @@ import pygame
 from pygame.locals import *
 import math
 import flatclasses as fcls
-    
+import random    
 
-
+#TODO: Hacer que cambie de escenario si se sale hacia la derecha.
 MARGIN = 3
 WINH, WINW = 700 +2*MARGIN, 1300 + 2*MARGIN
 BTLFLDH, BTLFLDW = 700, 900
+BASEW, BASEH = 50, 100
 
+ALLYBASEX, ALLYBASEY =0,250
+ENEMYBASEX, ENEMYBASEY = BTLFLDW-50, 250
+ENEMYCOLOR = (255,255,0)
+ALLYCOLOR = (0,0,255)
 BLACK = (0,0,0,0)
 GRAY = (150,150,150,0)
 DEEPBLUE = (0,4,130,0)
 
 
 
-def clearSurf(surf):
+def clearSurf(surf, backSurf):
     #dibujar las líneas
     surf.fill(BLACK)
     for ypoint in range(int(BTLFLDH/50)):
         pygame.draw.line(surf, GRAY, (0,50*ypoint),(BTLFLDW, 50*ypoint))
-    for xpoint in range(int(BTLFLDW/25)):
+    for xpoint in range(int(BTLFLDW/50)):
         pygame.draw.line(surf, GRAY, (50*xpoint,0), (50*xpoint, BTLFLDH))
+    
+    backSurf.blit(surf, (MARGIN, MARGIN))
+    
+    pygame.display.flip()
+    
     
 #Init
 pygame.init()
@@ -35,14 +45,18 @@ mainWindow.fill(DEEPBLUE)
 #Superficies
 #Campo de Batalla
 btlfldSurf = pygame.Surface((BTLFLDW, BTLFLDH)).convert()
-btlfldSurf.fill(BLACK)
-
-
-myShip = fcls.ship(newPos = (350,450), newColor =(255,0,0,0))
-fcls.objectsToDraw.append(myShip)
+#Bases
+allyBase = fcls.base(ALLYCOLOR, (ALLYBASEX, ALLYBASEY))
+enemyBase = fcls.base(ENEMYCOLOR, (ENEMYBASEX, ENEMYBASEY), isEnemy = True)
+#Crea las naves
+myShip = fcls.ship(newPos = (100,350), newColor =(0,0,255))
+ally1 = fcls.nship(newPos = (ALLYBASEX + 100, ALLYBASEY - 100), newColor = ALLYCOLOR)
+ally2 = fcls.nship(newPos = (ALLYBASEX +150, ALLYBASEY + 100), newColor = ALLYCOLOR)
+enemy1=fcls.nship(newPos = (ENEMYBASEX -100, ENEMYBASEY - 100), newColor = ENEMYCOLOR)
+enemy2=fcls.nship(newPos = (ENEMYBASEX -50, ENEMYBASEY + 100), newColor =ENEMYCOLOR)
+#Blit todo antes de empezar.
 mainWindow.blit(btlfldSurf, (MARGIN, MARGIN))
 pygame.display.flip()
-
 
 #Dibujar Superficie.
 clock = pygame.time.Clock()
@@ -51,14 +65,14 @@ def main():
     keep = True
     frame = 0
     while keep:
-        clearSurf(btlfldSurf)
-        for thing in fcls.objectsToDraw:
-            if not(0<=thing.getPos()[0]<= BTLFLDW)or not(0<=thing.getPos()[1]<= BTLFLDH):
-                fcls.objectsToDraw.remove(thing)
-                continue 
-            thing.calcPos()
-            thing.draw(btlfldSurf, mainWindow)
         
+        toDraw = []
+        clearSurf(btlfldSurf, mainWindow)#Limpia la superficie.
+        #Dibuja todos los objetos
+        for thing in fcls.inGame:
+            toDraw.append(thing.draw(btlfldSurf))
+        mainWindow.blit(btlfldSurf, (MARGIN, MARGIN))
+        pygame.display.update(toDraw)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 keep = False
@@ -66,19 +80,17 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key in list(myShip.dictAccs.keys()):
                     if event.key == pygame.K_w:
-                        #print('Arriba')
-                        myShip.velUp()
+                        myShip.velUp(1)
                     elif event.key == pygame.K_s:
-                        #print('Abajo')
-                        myShip.velDown()
+                        myShip.velDown(1)
                     elif event.key == pygame.K_d:
-                        #print('Derecha')
-                        myShip.velRight()
+                        myShip.velRight(1)
                     elif event.key == pygame.K_a:
-                        #print('Izquierda')
-                        myShip.velLeft()
+                        myShip.velLeft(1)
+                elif event.key ==pygame.K_SPACE:
+                    myShip.misil(pygame.mouse.get_pos())
             if event.type == pygame.MOUSEBUTTONDOWN:
-                myShip.shot()
+                myShip.shot(pygame.mouse.get_pos())
         clock.tick(60)
         frame = frame +1
     pygame.quit()
